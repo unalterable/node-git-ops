14b1f521a21218e912a0b6a98264fcc73f8275d5
Oliver Diamond <oliver.diamond@sky.uk>
2018-10-10 17:10:58 +0100

    Cleaning jenkins module

--- src/jenkins/index.js
+++ src/jenkins/index.js
@@ -1,40 +1,39 @@
 const fs = require('fs');
 const Mustache = require('mustache');
-
 const Jenkins = require('jenkins');
 
 const readFile = name => fs.readFileSync(__dirname + '/' + name).toString();
-
 const folderConfig = () => readFile('config-folder.xml');
 const pipelineJobConfig = vars => Mustache.render(readFile('config-pipeline-job.xml'), vars);
 
-const defaultOptions = {
-  gitRepoUrl: 'https://github.com/unalterable/base-webpack-express-app.git',
-  prepScript: 'docker',
-  testScript: 'npm test',
-  buildScript: 'docker',
-};
+const defaultOptions = { prepScript: 'docker', testScript: 'npm test', buildScript: 'docker' };
 
 const jenkinsClient = ({ host, username, password }) => {
-  let jenkins = Jenkins({baseUrl: `http://${username}:${password}@${host}`, crumbIssuer: true, promisify: true })
+  let jenkins = Jenkins({
+    baseUrl: `http://${username}:${password}@${host}`,
+    crumbIssuer: true,
+    promisify: true,
+  });
 
   const thisJenkins = {
-
-    info: () => jenkins.info({depth: 2}),
-
-    findFolder: async (folder) => (await thisJenkins.info()).jobs.find(({ jobs, name }) => jobs && name === folder),
-
-    createFolder: name => jenkins.job.create(name, folderConfig()),
-
+    info: () => jenkins.info({ depth: 2 }),
+    triggerBuild: (job) => jenkins.job.build(job),
+    destroyJob: (projectName, jobName) => jenkins.job.destroy(`${projectName}/${jobName}`),
+    destroyFolder: (projectName, jobName) => jenkins.job.destroy(projectName),
+    createFolder: (name) => jenkins.job.create(name, folderConfig()),
     createPipelineJob: (name, vars) => jenkins.job.create(name, pipelineJobConfig(vars)),
-
+    findFolder: async (folder) => {
+      const info = await thisJenkins.info();
+      return info.jobs.find(({ jobs, name }) => jobs && name === folder)
+    },
+    createFolderIfExists: async (name) => {
+      const folder = await thisJenkins.findFolder(name);
+      if (!folder) await thisJenkins.createFolder(name);
+    },
     createProjectJob: async (projectName, jobName, options) => {
       const job = `${projectName}/${jobName}`;
-      try{
-        const folder = await thisJenkins.findFolder(projectName);
-        if(!folder){
-          await thisJenkins.createFolder(projectName);
-        }
+      try {
+        await thisJenkins.createFolderIfExists(projectName);
         await thisJenkins.createPipelineJob(job, {...defaultOptions, ...options});
         await thisJenkins.triggerBuild(job)
       }
@@ -42,13 +41,6 @@ const jenkinsClient = ({ host, username, password }) => {
         throw Error(`Could not create job '${job}'. ` + e.message);
       }
     },
-
-    triggerBuild: job => jenkins.job.build(job),
-
-    destroyJob: (projectName, jobName) => jenkins.job.destroy(`${projectName}/${jobName}`),
-
-    destroyFolder: (projectName, jobName) => jenkins.job.destroy(projectName),
-
   };
 
   return thisJenkins;
