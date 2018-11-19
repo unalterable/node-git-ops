35447882ebbdc1edceff69937ceb0b75401ceb63
unalterable <diamond.oliver@gmail.com>
2018-11-19 21:53:38 +0000

    better versioning

--- src/docker-hub/index.js
+++ src/docker-hub/index.js
@@ -1,28 +1,35 @@
+const _ = require('lodash');
 const axios = require('axios');
 
-const incrementVersion = majorMinorPatch => ({ major, minor, patch}) => {
+const incrementVersion = majorMinorPatch => ({ major, minor, patch }) => {
   switch(majorMinorPatch) {
     case 'major':
-      return `${parseInt(major) + 1}.0.0`
+      return `${major + 1}.0.0`
     case 'minor':
-      return `${major}.${parseInt(minor) + 1}.0`
+      return `${major}.${minor + 1}.0`
     case 'patch':
     default:
-      return `${major}.${minor}.${parseInt(patch) + 1}`
+      return `${major}.${minor}.${patch + 1}`
   }
 }
 
 const getNextVersion = ({ dockerHubRepo, increment }) => {
   return axios.get(`https://index.docker.io/v1/repositories/${dockerHubRepo}/tags`)
-    .then(({data}) => data
-      .sort()
+    .then(({data}) => _(data)
       .map(({ name }) => name.match(/^([0-9])*\.([0-9]*)\.([0-9]*)$/))
       .filter(match => match)
-      .pop()
+      .map(match => ({
+        major: parseInt(match[1]),
+        minor: parseInt(match[2]),
+        patch: parseInt(match[3]),
+      }))
+      .sortBy(['major', 'minor', 'patch'])
+      .last()
     )
-    .then(([_, major, minor, patch]) => ({ major, minor, patch }))
+    .then(currentVersion => currentVersion || { major: 0, minor: 0, patch: 0 })
     .then(incrementVersion(increment))
     .then(console.log)
+    .catch(console.error)
 }
 
 module.exports = { getNextVersion };
