aca60f39bf0204ff903950b254c6cf76333db3bd
unalterable <diamond.oliver@gmail.com>
2018-12-15 14:53:45 +0000

    get next version from docker hub function name

--- src/jenkins/config-templates/default-scripts/build-step.sh
+++ src/jenkins/config-templates/default-scripts/build-step.sh
@@ -2,7 +2,7 @@ GIT_OPS_DIR=node_git_ops_tools
 git clone https://github.com/unalterable/node-git-ops.git $GIT_OPS_DIR
 cd $GIT_OPS_DIR
 npm i
-version=$(node -e "require('./src/docker/hub').getNextVersion({ dockerHubRepo: '{{{ imageName }}}', increment: '$versionIncrement' })")
+version=$(node -e "require('./src/docker/hub').getNextVersionFromHub({ dockerHubRepo: '{{{ imageName }}}', increment: '$versionIncrement' })")
 cd ..
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
