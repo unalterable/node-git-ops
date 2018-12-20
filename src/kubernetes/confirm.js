5627ae543115357246c2fa1fb94869cf6e12b867
Oliver Diamond <oliver.diamond@sky.uk>
2018-12-20 09:07:26 +0000

    attempting different identifier for deployment confirmation

--- src/kubernetes/confirm.js
+++ src/kubernetes/confirm.js
@@ -1,11 +1,14 @@
 const shell = require('shelljs');
 const config = require('../../deployment-config.json');
 
-const rolloutStatusCommand = `kubectl --kubeconfig=$KUBECONF rollout status deploy ${config.applicationName}`;
+const kubeConf = '--kubeconfig=$KUBECONF';
+const namespace = `--namespace=${config.namespace}`;
+const rolloutStatusCommand = `kubectl ${kubeConf} ${namespace} rollout status deployment ${config.applicationName}`;
 
 console.info('=============');
 
 console.info(rolloutStatusCommand);
 shell.exec(rolloutStatusCommand);
+if (shell.error()) throw Error('Failed');
 
 console.info('============= Done');
